68dabadbd242ed6d66b4e653347b554878264263
anilceyhanyeni <anilceyhanyeni@gmail.com>
2023-04-09 20:39:54 +0300

    feat: useref ornegi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useRef } from "react";
 
 function App() {
+  const renkInput = useRef()
+
+  function renkDegistir() {
+    const renk = renkInput.current.value
+    document.body.style.backgroundColor = renk
+  }
+
+
   return (
-<div>App</div>
+<div>
+  <input ref={renkInput} type="text" />
+  <button onClick={renkDegistir}>Renk Değiştir</button>
+</div>
   );
 }
 
