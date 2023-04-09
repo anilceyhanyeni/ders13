640200592e3e0a804a873aef18e679211f829a4a
anilceyhanyeni <anilceyhanyeni@gmail.com>
2023-04-09 16:12:23 +0300

    feat: react fetch ornegi yapildi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
 
 function App() {
+
+  let [dogImage, setDogImage] = useState(null)
+
+  useEffect(() => {
+    /*
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => setDogImage(data.message))
+    */
+
+    async function gorselCek() {
+      const yanit = await fetch("https://dog.ceo/api/breeds/image/random")
+      const veri = await yanit.json()
+
+      setDogImage(veri.message)
+    }
+
+    gorselCek()
+
+  }, [])
+
   return (
-<div>App</div>
+    <div>
+      { dogImage && <img src={dogImage} alt="" /> }
+    </div>
   );
 }
 
