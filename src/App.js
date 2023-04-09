907344e378bb96cfd8d6ae96f9fc3177e367f423
anilceyhanyeni <anilceyhanyeni@gmail.com>
2023-04-09 20:23:01 +0300

    feat  useref basit kullanımı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-
+import { useRef, useState } from "react";
 function App() {
+
+  const [sayi, sayiGuncelle] = useState(0)
+  
+  const ref1 = useRef(1)
+
+  if ( sayi > 5) {
+    ref1.current = 5
+  }
+
+  console.log(ref1)
+
   return (
-<div>App</div>
+    <div>
+      {sayi}
+      <button onClick={ () => {sayiGuncelle(oncekiSayi => oncekiSayi +1) }}>Arttır</button>
+    </div>
   );
 }
 
