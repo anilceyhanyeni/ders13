c39544280f00cc0a37e38f887ac8ea626fa25001
anilceyhanyeni <anilceyhanyeni@gmail.com>
2023-04-09 18:34:16 +0300

    feat: fetch ornegi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
+import { useEffect, useState } from "react";
+import Karakter from "./Karakter";
+import Filmler from "./Filmler";
 
 function App() {
+
+  const [karakter, karakterGuncelle] = useState(null);
+  const [kisiNo, kisiNoGuncelle] = useState("")
+  const [hata, hataGuncelle] = useState(null)
+
+  const veriCek = async () => {
+    if (kisiNo) {
+      const response = await fetch(`https://swapi.dev/api/people/${kisiNo}/`);
+
+      if (response.status === 404) { //kişi bulunamadı
+        hataGuncelle("Kişi bulunamadı")
+        karakterGuncelle(null)
+      } else {
+        const veri = await response.json();
+        karakterGuncelle(veri)
+        hataGuncelle(null)
+      }
+    }
+  };
+
+  useEffect(() => {
+    veriCek();
+  }, []);
+
+
+  function aramaYap() {
+    veriCek()
+  }
+
+
   return (
-<div>App</div>
-  );
+
+    <div className="container">
+      <div className="row">
+        <input value={kisiNo} onChange={(olay) => { kisiNoGuncelle(olay.target.value) }} type="number" />
+        <button onClick={aramaYap}>Bul</button>
+      </div>
+
+      {hata ? hata : ""}
+
+      {karakter ?
+        (<div className="row">
+          <Karakter person={karakter} />
+          <Filmler filmler={karakter.films} />
+        </div>)
+        :
+        (<div>
+          Lütfen bir kişi no girin ve arama butonuna basın.
+        </div>)
+      }
+
+    </div>
+
+  )
+
 }
 
 export default App;
